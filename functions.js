c08cabf12ea5e82fb5185f08596e528fce241487
DarkMalk <sebastian.cea40@gmail.com>
2023-11-16 12:09:03 -0300

    🆕 feat: Add funcs for the ping and generate ips

---
+++ functions.js
@@ -0,0 +1,38 @@
+import { exec } from 'node:child_process'
+import { promisify } from 'node:util'
+
+const execAsync = promisify(exec)
+
+/**
+ *
+ * @param {string} ipSegment
+ * @param {string} netmask
+ * @returns {string[]}
+ */
+export function generateIPs(ipSegment, netmask) {
+  const availableIPs = Math.pow(2, 32 - parseInt(netmask, 10))
+  let ipBase = ipSegment
+    .split('.')
+    .slice(0, 3)
+    .map(segment => parseInt(segment))
+  const IPs = []
+
+  let lastSegmentIp = 0
+  for (let i = 1; i <= availableIPs; i++) {
+    if (lastSegmentIp > 255) {
+      ipBase[ipBase.length - 1]++
+      lastSegmentIp = 0
+    }
+    IPs.push(`${ipBase.join('.')}.${lastSegmentIp}`)
+    lastSegmentIp++
+  }
+  return IPs
+}
+
+/**
+ *
+ * @param {string} ip
+ */
+export async function pingIP(ip) {
+  return execAsync(`ping -c 1 ${ip} -W 1000`)
+}
